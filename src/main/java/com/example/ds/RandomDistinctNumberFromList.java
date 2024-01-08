a3610adf3a4b729f5f63c989b611a5337bfc5448
Shivani <shivani.2598@gmail.com>
2024-01-08 14:36:41 +0530

    Display distinct elements from a list and randomly display them

---
+++ src/main/java/com/example/ds/RandomDistinctNumberFromList.java
@@ -0,0 +1,23 @@
+/*
+
+package com.example.ds;
+
+
+import java.util.*;
+import java.util.stream.Collectors;
+import javax.print.attribute.IntegerSyntax;
+import javax.swing.*;
+class RandomNumber {
+    public static void main(String[] args) {
+       List<Integer> numbers = new ArrayList<>();
+       Scanner sc = new Scanner(System.in);
+       while (sc.hasNextInt()) {
+           numbers.add(sc.nextInt());
+       }
+       List<Integer> distinct = new ArrayList<>();
+       distinct.addAll(numbers.stream().distinct().collect(Collectors.toList()));
+       Collections.shuffle(distinct);
+       System.out.println(distinct);
+    }
+}
+*/
